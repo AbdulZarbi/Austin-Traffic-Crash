daffc1d40d6df7d9fd29fda9e8b2292adb2a0023
AWZ <57395474+AbdulZarbi@users.noreply.github.com>
2025-07-22 13:42:27 -0500

    Updated

--- Code.R
+++ Code.R
@@ -3,3 +3,11 @@ setwd("/Users/zarbi/Documents/GitHub/Austin_Traffic_Crash")
 getwd()
 
 #load the dataset 
+library(readr)
+Austin_Crash <- read.csv("~/Downloads/AustinTrafficCrash.csv")
+
+#Basic Dataset Overview
+
+View(Austin_Crash)
+colnames(Austin_Crash)
+head(Austin_Crash)
